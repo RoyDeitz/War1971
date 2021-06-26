c5b098f06d38529da4462f3e7682242cd944a995
RoyDevashis <roydeitz@gmail.com>
2021-06-26 03:53:36 +0200

    Text Mesh pro installation

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    public struct Gun
+    {
+        public bool available;
+        public string name;
+        public int firingRate;
+        public int magCapacity;
+        public int currentMag;
+        public int currentAmmo;
+        public int maxAmmo;
+    }
+
+    public Gun[] guns;
     // Start is called before the first frame update
     void Start()
     {
